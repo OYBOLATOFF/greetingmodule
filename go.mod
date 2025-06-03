f4cf8d77ef9c26b5ddf2be1d2559c2a646f9e17f
Ramazan Oybolatov <ramazanoybolatov@gmail.com>
2025-06-03 21:26:13 +0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/OYBOLATOFF/greetingmodule
+
+go 1.22.4
---
+++ greeting.go
@@ -0,0 +1,22 @@
+package greeting
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func Hello() {
+	fmt.Println("Hello from module!")
+}
+
+func GreetByName(name string) {
+	fmt.Printf("Hello, %s", name)
+}
+
+func AskNameAndGreet() {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("Как тебя зовут: ")
+	name, _ := reader.ReadString('\n')
+	GreetByName(name)
+}
